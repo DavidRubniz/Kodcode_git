8d9b0fc7ae3375aa59e7b8a20f523f0ac422ff5c
d0504140924-max <d0504140924@gmail.com>
2026-05-13 09:10:55 +0300

    added a hungman game project

---
+++ week_5/python_project/hung_man.py
@@ -0,0 +1,71 @@
+import random
+
+WORDS_LIST = ['red', 'blue', 'green', 'grey']
+guess = []
+
+def random_word():
+    return random.choice(WORDS_LIST)
+
+
+def get_valid_input():
+    while True:
+        gues = input('Enter your gues: ').lower()
+        if ('a' <= gues <= 'z' or 'A' <= gues <= 'Z') and len(gues) == 1:
+            return gues
+        print('you must enter one letter in English')
+
+
+def check_gues(word: str, gues: str):
+    if gues in word:
+        if not gues in guess:
+            return True, word.index(gues)
+        else:
+            letter_count = word.count(gues)
+            if letter_count > 1:
+                indexes = [i for i, letter in enumerate(word) if letter == gues]
+                guess_count = guess.count(gues)
+                return True, indexes[guess_count]
+    return False
+
+
+def check_finish(_status: list, _turns: int):
+    if _turns == 0:
+        print('you failed')
+        return True
+    if '_' not in _status:
+        print(f'well done the word is: {''.join(_status)}')
+        return True
+    return False
+
+def main():
+    print('wellcome to hung-man game')
+    word = random_word()
+    status = ['_' for i in range(len(word))]
+    turns = 5
+    while True:
+        print(''.join(status))
+        print(f'you left {turns} turns to play')
+        print(guess)
+        val_inp = get_valid_input()
+        sec = check_gues(word, val_inp)
+        guess.append(val_inp)
+        if sec:
+            status[sec[1]] = val_inp
+        else:
+            turns -= 1
+         if check_finish(status, turns):
+             break
+
+if __name__ == "__main__":
+    while True:
+        main()
+        _choice = input('press 0 for exit and 1 to play again: ')
+        if _choice == '0':
+            break
+        print()
+        print()
+
+
+
+
+
